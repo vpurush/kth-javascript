45872fc3976bcc1c6c6b07de6768bd3f0e979f09
Vijayaraghavan P <188923@cognizant.com>
2022-09-25 11:37:58 +1000

    authenticate api calls

--- packages/kth-website/pages/api/hello.js
+++ packages/kth-website/pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withApiAuthRequired } from '@auth0/nextjs-auth0';
 
-export default function handler(req, res) {
+function protectedHelloHandler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+export default withApiAuthRequired(protectedHelloHandler);
